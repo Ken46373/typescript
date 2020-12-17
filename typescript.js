35f6bd87f11c361575268d98e74d6a0d64f857b5
Ken Minami <kenminami.dev@gmail.com>
2020-12-17 22:27:58 +0900

    project setup.

--- typescript.js
+++
@@ -1,5 +0,0 @@
-var sum = function (a, b) {
-    return a + b;
-};
-var ANSWER = sum(4, 5);
-console.log(ANSWER);
--- typescript.ts
+++
@@ -1,6 +0,0 @@
-const sum = (a : number, b : number) => {
-    return a + b;
-}
-
-const ANSWER = sum(4 , 5);
-console.log(ANSWER);
