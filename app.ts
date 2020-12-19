74d2feb132dad930a5b1d2184924970bd5334bf7
Ken Minami <kenminami.dev@gmail.com>
2020-12-19 22:45:37 +0900

    type: Array

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
 const person = {
     name: 'ken',
     age: 26,
+    hobbies: ['Sports', 'Cooking'],
 };
 
 console.log(person);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
