6cfbdad9a41f8399adbaa9d26bbfedf6f2d8ebc4
Ken Minami <kenminami.dev@gmail.com>
2020-12-20 23:29:03 +0900

    type: tuple

--- app.ts
+++ app.ts
@@ -1,8 +1,16 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'ken',
     age: 26,
     hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'],
 };
+
+person.role　= [1, 'admin']
 
 console.log(person);
 
