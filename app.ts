fec70df85228c79fae14646e7be99c222676752a
Ken Minami <kenminami.dev@gmail.com>
2020-12-21 20:35:00 +0900

    type: enum

--- app.ts
+++ app.ts
@@ -1,19 +1,18 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
-    name: 'ken',
-    age: 26,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author'],
-};
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
 
-person.role　= [1, 'admin']
+const person = {
+  name: "ken",
+  age: 26,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
 
 console.log(person);
 
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
+if (person.role == Role.ADMIN) {
+    console.log("管理者ユーザ");
 }
