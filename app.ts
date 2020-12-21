59aeb59c8ce8918163b9a61e18973aa793ad37ae
Ken Minami <kenminami.dev@gmail.com>
2020-12-21 20:53:20 +0900

    type: alias

--- app.ts
+++ app.ts
@@ -1,6 +1,16 @@
-function combine(input1: number | string, input2: number | string, resultConversion: 'as-text' | 'as-number') {
+type Comvinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
+function combine(
+  input1: Comvinable,
+  input2: Comvinable,
+  resultConversion: ConversionDescriptor
+) {
   let result;
-  if ((typeof input1 === "number" && typeof input2 === "number") || resultConversion === 'as-number') {
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
     result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
@@ -8,11 +18,11 @@ function combine(input1: number | string, input2: number | string, resultConvers
   return result;
 }
 
-const combinedAges = combine(26, 22, 'as-number');
+const combinedAges = combine(26, 22, "as-number");
 console.log(combinedAges);
 
-const combinedStringAges = combine(26, 22, 'as-number');
+const combinedStringAges = combine(26, 22, "as-number");
 console.log(combinedStringAges);
 
-const combinedNames = combine("Max", "Anna", 'as-text');
+const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
