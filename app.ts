b7fa3469578be2d06815a089d2ac3fb3572b4649
Ken Minami <kenminami.dev@gmail.com>
2020-12-21 20:49:33 +0900

    type: literal

--- app.ts
+++ app.ts
@@ -1,16 +1,18 @@
-function combine(input1: number | string, input2: number | string) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString()
-    }
-    return result;
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-text' | 'as-number') {
+  let result;
+  if ((typeof input1 === "number" && typeof input2 === "number") || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-const combinedAges = combine(26, 22);
+const combinedAges = combine(26, 22, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Max', 'Anna');
+const combinedStringAges = combine(26, 22, 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", 'as-text');
 console.log(combinedNames);
