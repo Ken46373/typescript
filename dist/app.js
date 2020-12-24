39aa7a59d1a46c051c03d7f34794f6d6c0142fdc
Ken Minami <kenminami.dev@gmail.com>
2020-12-24 21:40:45 +0900

    update class.

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,24 @@
 "use strict";
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    describe() {
+        console.log(`Department: (${this.id}): ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department('d1', 'Accounting');
+accounting.describe();
+accounting.addEmployee('Max');
+accounting.addEmployee('Rina');
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 class Department {
-    name: string;
+    private employees: string[] = [];
 
-    constructor(n: string) {
-        this.name  = n;
+    constructor(private readonly id: string, public name: string) {
+    }
+
+    describe(this: Department) {
+        console.log(`Department: (${this.id}): ${this.name}`);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
+accounting.describe();
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Rina');
+
+accounting.printEmployeeInformation()
