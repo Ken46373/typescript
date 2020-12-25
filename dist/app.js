2395cb58e6ca5194e5a8a50a6727ad22c0d2b52f
Ken Minami <kenminami.dev@gmail.com>
2020-12-25 22:23:08 +0900

    class: getter/setter

--- dist/app.js
+++ dist/app.js
@@ -16,28 +16,49 @@ class Department {
         console.log(this.employees);
     }
 }
-class itDepartment extends Department {
+class ItDepartment extends Department {
     constructor(id, admins) {
         super(id, 'it');
         this.admins = admins;
         this.admins = admins;
     }
 }
-class accontingDepartment extends Department {
+class accountingDepartment extends Department {
     constructor(id, reports) {
-        super(id, 'acconting');
+        super(id, 'accounting');
         this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get getLastReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("レポートが見つかりません。");
+    }
+    set setLastReport(value) {
+        if (!value) {
+            throw new Error("正しい値を設定してください。");
+        }
+        this.addReports(value);
     }
     addReports(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     printReports() {
         console.log(this.reports);
     }
+    addEmployee(name) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
 }
-const it = new itDepartment('d1', ['Max']);
+const it = new ItDepartment('d1', ['Max']);
 console.log(it);
-const acconting = new accontingDepartment('d2', []);
-acconting.addReports("something");
-acconting.printReports();
+const accounting = new accountingDepartment('d2', []);
+accounting.addReports("something");
+console.log(accounting.getLastReport);
+accounting.printReports();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 class Department {
-    private employees: string[] = [];
+    protected employees: string[] = [];
 
     constructor(private readonly id: string, public name: string) {
     }
@@ -18,30 +18,57 @@ class Department {
     }
 }
 
-class itDepartment extends Department {
+class ItDepartment extends Department {
     constructor(id: string, private admins: string[]) {
         super(id, 'it');
         this.admins = admins;
     }
 }
 
-class accontingDepartment extends Department {
+class accountingDepartment extends Department {
+
+    private lastReport: string;
+    
     constructor(id: string, private reports: string[]) {
-        super(id, 'acconting');
+        super(id, 'accounting');
+        this.lastReport = reports[0];
+    }
+
+    get getLastReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("レポートが見つかりません。");
+    }
+
+    set setLastReport(value: string) {
+        if (!value) {
+            throw new Error("正しい値を設定してください。");
+        }
+        this.addReports(value);
     }
 
     addReports(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
 
     printReports() {
         console.log(this.reports);
     }
+
+    addEmployee(name: string) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
 }
 
-const it = new itDepartment('d1', ['Max']);
+const it = new ItDepartment('d1', ['Max']);
 console.log(it);
 
-const acconting = new accontingDepartment('d2', []);
-acconting.addReports("something");
-acconting.printReports();
+const accounting = new accountingDepartment('d2', []);
+accounting.addReports("something");
+console.log(accounting.getLastReport);
+accounting.printReports();
