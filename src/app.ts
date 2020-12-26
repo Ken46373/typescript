752e18aabd228e0707314d130a6fdd64d21de661
Ken Minami <kenminami.dev@gmail.com>
2020-12-26 22:13:00 +0900

    type: 交差型

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,17 @@
-interface AddFn {
-    (n1: number, n2: number): number;
+type Admin = {
+    name: string;
+    privileges: string[];
 }
 
-let add: AddFn;
-add = (n1: number, n2: number) => {
-    return n1 + n2;
+type Employee = {
+    name: string;
+    startDate: Date;
 }
 
-interface Named {
-    readonly name?: string;
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date(),
 }
-
-interface Greetable extends Named {
-    greet(phrase: string): void;
-}
-
-class Person implements Greetable {
-    name?: string;
-    age = 30;
-
-    constructor(n?: string) {
-        if (n) {
-            this.name = n;
-        }
-    }
-
-    greet(phrase: string) {
-        if (this.name) {
-            console.log(phrase + ' ' + this.name);
-        }
-        else {
-            console.log('Hi!!');
-        }
-    }
-}
-
-let user1: Greetable;
-
-user1 = new Person();
-user1.greet('Hello I am');
-console.log(user1);
