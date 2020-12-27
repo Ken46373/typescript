52d22bf0aad52dc817ceb360ee7b3a6c5a93c746
Ken Minami <kenminami.dev@gmail.com>
2020-12-27 21:56:51 +0900

    generics: keyof

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,28 @@
-
 function merge<T extends object, U extends object>(obj1: T, obj2: U) {
-    return Object.assign(obj1, obj2);
+  return Object.assign(obj1, obj2);
 }
 
-const mergedObj = merge({name: 'Max'}, {age: 30});
+const mergedObj = merge({ name: "Max" }, { age: 30 });
 
 interface Lengthy {
-    length: number;
+  length: number;
 }
 
 function countAndDescription<T extends Lengthy>(element: T): [T, string] {
-    let descriptionText = '値がありません。';
-    if (element.length > 0) {
-        descriptionText = '値は' + element.length + '個です。';
-    }
-    return [element, descriptionText];
+  let descriptionText = "値がありません。";
+  if (element.length > 0) {
+    descriptionText = "値は" + element.length + "個です。";
+  }
+  return [element, descriptionText];
 }
 
-console.log(countAndDescription(['Sports', 'Cooking']));
+console.log(countAndDescription(["Sports", "Cooking"]));
+
+function extractrAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+extractrAndConvert({ name: "Max" }, "name");
