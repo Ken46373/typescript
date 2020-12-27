2bf4095b088c7fa54c16f8880eee3e131a4e0bbb
Ken Minami <kenminami.dev@gmail.com>
2020-12-27 21:53:08 +0900

    update generics.

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,17 @@ function merge<T extends object, U extends object>(obj1: T, obj2: U) {
 }
 
 const mergedObj = merge({name: 'Max'}, {age: 30});
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescription<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = '値がありません。';
+    if (element.length > 0) {
+        descriptionText = '値は' + element.length + '個です。';
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescription(['Sports', 'Cooking']));
