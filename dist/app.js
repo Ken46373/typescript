be43b2e7de98aeace91ce8702846d1da05d41428
Ken Minami <kenminami.dev@gmail.com>
2020-12-29 21:26:08 +0900

    フォーム表示

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,15 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-function Logger(logString) {
-    return function (constructor) {
-        console.log(logString);
-        console.log(constructor);
-    };
-}
-let Person = class Person {
+class ProjectInput {
     constructor() {
-        this.name = 'Max';
-        console.log('Personオブジェクト作成中...');
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
     }
-};
-Person = __decorate([
-    Logger('ログ出力 - Person')
-], Person);
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const prj = new ProjectInput();
 //# sourceMappingURL=app.js.map
