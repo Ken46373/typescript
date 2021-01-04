11bed857824997241751926dd8be72ba4a756586
Ken Minami <kenminami.dev@gmail.com>
2021-01-04 22:44:37 +0900

    Update project-input.ts

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,5 +1,5 @@
 import { Component } from "./base-component.js";
-import { Validatable, validate } from "../utils/validation.js";
+import * as Validation from "../utils/validation.js";
 import { autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
 
@@ -36,25 +36,25 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredManday = this.mandayInputElement.value;
 
-    const titleValidatable: Validatable = {
+    const titleValidatable: Validation.Validatable = {
       value: enteredTitle,
       required: true,
     };
-    const descriptionValidatable: Validatable = {
+    const descriptionValidatable: Validation.Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5,
     };
-    const mandayValidatable: Validatable = {
+    const mandayValidatable: Validation.Validatable = {
       value: +enteredManday,
       required: true,
       min: 1,
       max: 1000,
     };
     if (
-      !validate(titleValidatable) ||
-      !validate(descriptionValidatable) ||
-      !validate(mandayValidatable)
+      !Validation.validate(titleValidatable) ||
+      !Validation.validate(descriptionValidatable) ||
+      !Validation.validate(mandayValidatable)
     ) {
       alert("入力値が正しくありません。再度お試しください。");
       return;
