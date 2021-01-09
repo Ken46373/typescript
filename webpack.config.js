db8a39b2f2a087ff40c94a9a7e2c415c13374e50
Ken Minami <kenminami.dev@gmail.com>
2021-01-09 23:43:41 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ module.exports = {
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        publicPath: "/dist",
     },
     devtool: 'inline-source-map',
     module: {
